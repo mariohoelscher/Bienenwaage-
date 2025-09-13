567b483b56d618945cd9b5131a87f2ddb30a96c1
mariohoelscher <65395356+mariohoelscher@users.noreply.github.com>
2025-09-13 23:14:19 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,5 @@
-self.addEventListener('install', e=>{ e.waitUntil(caches.open('v1').then(cache=>cache.addAll(['./','./index.html','./manifest.json']))); });
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+const CACHE_NAME = "hoelschers-bienen-v1";
+const FILES_TO_CACHE = ["./","./index.html","./manifest.json"];
+self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)));self.skipWaiting();});
+self.addEventListener('activate', e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(k!==CACHE_NAME)return caches.delete(k);})))));self.clients.claim();});
+self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
