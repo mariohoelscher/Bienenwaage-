644e7bad649338fd2d0cf73f7f771ca89d79b6eb
mariohoelscher <65395356+mariohoelscher@users.noreply.github.com>
2025-09-13 12:17:39 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,8 @@
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('v1').then(cache => {
-    return cache.addAll(['/', '/index.html', '/manifest.json']);
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('app-cache').then(cache => {
+    return cache.addAll(['./','./index.html','./manifest.json']);
   }));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => {
-    return response || fetch(event.request);
-  }));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
