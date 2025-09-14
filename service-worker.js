f94197a052cc3526ef62075d6ec72f50289f1820
mariohoelscher <65395356+mariohoelscher@users.noreply.github.com>
2025-09-14 16:40:06 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,19 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+const CACHE_NAME = "bienen-app-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
